e35b331863f9142d740b0460bc8d11b44d15d55c
alessandro moreira <66788307+Alessandro-HCL@users.noreply.github.com>
2025-04-06 17:15:51 -0300

    Update contagem_estoque_com_streamlit_valor.py

--- contagem_estoque_com_streamlit_valor.py
+++ contagem_estoque_com_streamlit_valor.py
@@ -683,6 +683,7 @@ for categoria, itens in categorias_estoque.items():
     with st.expander(f"📂 {categoria.title()}"):
         for item in itens:
             quantidade = st.number_input(f"{item}", min_value=0.0, step=0.1, key=item)
+            # quantidade = st.number_input(f"{item}", min_value=0.0, step=0.1, key=item)
             valor_unitario = valores_unitarios.get(item, 1.00)
             valor_total = round(quantidade * valor_unitario, 2)
 
