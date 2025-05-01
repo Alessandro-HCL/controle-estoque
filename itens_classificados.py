d3e4adf461462a19b7e20f2cbbf65de2242865d1
alessandro moreira <66788307+Alessandro-HCL@users.noreply.github.com>
2025-05-01 15:56:43 -0300

    Update itens_classificados.py

--- itens_classificados.py
+++ itens_classificados.py
@@ -2,7 +2,7 @@ itens_classificados = [
     ("492 - granola (KG)", "📦 Outros ou diversos"),
     ("597 - biscoito(CX)", "📦 Outros ou diversos"),
     # ("499 - açai(KG)", "📦 Outros ou diversos"),
-    ("499 - açai  (kg)", "📦 Outros ou diversos")
+    ("499 - açai  (kg)", "📦 Outros ou diversos"),
     ("496 - achocolatado (KG)", "📦 Outros ou diversos"),
     ("495 - leite em po (KG)", "🧀 Laticínios e Frios"),
     ("285 - presunto fatiado (KG)", "🧀 Laticínios e Frios"),
